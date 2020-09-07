32a44cdb3951317fd121bf00fb3ea68f2346cdf5
Harrison Holgate <harrisonrobertholgate@gmail.com>
2020-09-07 22:15:08 +0100

    Created initial folder structure.

---
+++ CMakeFiles/PhysicsEngine.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/PhysicsEngine.dir/src/main.cpp.o"
+  "PhysicsEngine"
+  "PhysicsEngine.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/PhysicsEngine.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
