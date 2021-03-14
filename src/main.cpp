b6399fce2ab8d26d8694609e92a759dec44bafc5
Harrison Holgate <h.holgate@lancaster.ac.uk>
2021-03-14 23:44:41 +0000

    Added lib and ability to build for windows.

--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,8 @@
 
 int main()
 {
-    Engine engine;
+    std::cout << "This is the first line of the program. \n";
+    Engine engine = Engine();
     std::cout << "Started engine from entry point [int main()].\n";
     engine.run();
     return 0;
