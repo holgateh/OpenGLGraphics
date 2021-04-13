5e12d8f266e8b0217c588885bbecc4c8752d8111
Harrison Holgate <h.holgate@lancaster.ac.uk>
2021-04-13 16:37:57 +0000

    Removed ability to bulid for windows as I've yet to refine the build process.

--- CMakeFiles/GameEngine.dir/DependInfo.cmake
+++ CMakeFiles/GameEngine.dir/DependInfo.cmake
@@ -1,39 +1,32 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
 # The set of languages for which implicit dependencies are needed:
 set(CMAKE_DEPENDS_LANGUAGES
-  "CXX"
   )
-# The set of files for implicit dependencies of each language:
-set(CMAKE_DEPENDS_CHECK_CXX
-  "C:/Users/harri/Documents/GameEngine/external/imgui/imgui.cpp" "C:/Users/harri/Documents/GameEngine/CMakeFiles/GameEngine.dir/external/imgui/imgui.cpp.obj"
-  "C:/Users/harri/Documents/GameEngine/external/imgui/imgui_demo.cpp" "C:/Users/harri/Documents/GameEngine/CMakeFiles/GameEngine.dir/external/imgui/imgui_demo.cpp.obj"
-  "C:/Users/harri/Documents/GameEngine/external/imgui/imgui_draw.cpp" "C:/Users/harri/Documents/GameEngine/CMakeFiles/GameEngine.dir/external/imgui/imgui_draw.cpp.obj"
-  "C:/Users/harri/Documents/GameEngine/external/imgui/imgui_impl_glfw.cpp" "C:/Users/harri/Documents/GameEngine/CMakeFiles/GameEngine.dir/external/imgui/imgui_impl_glfw.cpp.obj"
-  "C:/Users/harri/Documents/GameEngine/external/imgui/imgui_impl_opengl3.cpp" "C:/Users/harri/Documents/GameEngine/CMakeFiles/GameEngine.dir/external/imgui/imgui_impl_opengl3.cpp.obj"
-  "C:/Users/harri/Documents/GameEngine/external/imgui/imgui_widgets.cpp" "C:/Users/harri/Documents/GameEngine/CMakeFiles/GameEngine.dir/external/imgui/imgui_widgets.cpp.obj"
-  "C:/Users/harri/Documents/GameEngine/external/implot/implot.cpp" "C:/Users/harri/Documents/GameEngine/CMakeFiles/GameEngine.dir/external/implot/implot.cpp.obj"
-  "C:/Users/harri/Documents/GameEngine/external/implot/implot_demo.cpp" "C:/Users/harri/Documents/GameEngine/CMakeFiles/GameEngine.dir/external/implot/implot_demo.cpp.obj"
-  "C:/Users/harri/Documents/GameEngine/external/implot/implot_items.cpp" "C:/Users/harri/Documents/GameEngine/CMakeFiles/GameEngine.dir/external/implot/implot_items.cpp.obj"
-  "C:/Users/harri/Documents/GameEngine/src/Camera.cpp" "C:/Users/harri/Documents/GameEngine/CMakeFiles/GameEngine.dir/src/Camera.cpp.obj"
-  "C:/Users/harri/Documents/GameEngine/src/Engine.cpp" "C:/Users/harri/Documents/GameEngine/CMakeFiles/GameEngine.dir/src/Engine.cpp.obj"
-  "C:/Users/harri/Documents/GameEngine/src/Entity.cpp" "C:/Users/harri/Documents/GameEngine/CMakeFiles/GameEngine.dir/src/Entity.cpp.obj"
-  "C:/Users/harri/Documents/GameEngine/src/Light.cpp" "C:/Users/harri/Documents/GameEngine/CMakeFiles/GameEngine.dir/src/Light.cpp.obj"
-  "C:/Users/harri/Documents/GameEngine/src/RenderingEngine/Mesh.cpp" "C:/Users/harri/Documents/GameEngine/CMakeFiles/GameEngine.dir/src/RenderingEngine/Mesh.cpp.obj"
-  "C:/Users/harri/Documents/GameEngine/src/RenderingEngine/Renderer.cpp" "C:/Users/harri/Documents/GameEngine/CMakeFiles/GameEngine.dir/src/RenderingEngine/Renderer.cpp.obj"
-  "C:/Users/harri/Documents/GameEngine/src/Shader.cpp" "C:/Users/harri/Documents/GameEngine/CMakeFiles/GameEngine.dir/src/Shader.cpp.obj"
-  "C:/Users/harri/Documents/GameEngine/src/Texture.cpp" "C:/Users/harri/Documents/GameEngine/CMakeFiles/GameEngine.dir/src/Texture.cpp.obj"
-  "C:/Users/harri/Documents/GameEngine/src/Timer.cpp" "C:/Users/harri/Documents/GameEngine/CMakeFiles/GameEngine.dir/src/Timer.cpp.obj"
-  "C:/Users/harri/Documents/GameEngine/src/main.cpp" "C:/Users/harri/Documents/GameEngine/CMakeFiles/GameEngine.dir/src/main.cpp.obj"
-  )
-set(CMAKE_CXX_COMPILER_ID "GNU")
 
-# The include file search paths:
-set(CMAKE_CXX_TARGET_INCLUDE_PATH
-  "lib"
-  "include"
-  "external"
-  "external/imgui"
-  "external/implot"
-  "external/stb"
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/harrison/OpenGLGraphics/external/imgui/imgui.cpp" "CMakeFiles/GameEngine.dir/external/imgui/imgui.cpp.o" "gcc" "CMakeFiles/GameEngine.dir/external/imgui/imgui.cpp.o.d"
+  "/home/harrison/OpenGLGraphics/external/imgui/imgui_demo.cpp" "CMakeFiles/GameEngine.dir/external/imgui/imgui_demo.cpp.o" "gcc" "CMakeFiles/GameEngine.dir/external/imgui/imgui_demo.cpp.o.d"
+  "/home/harrison/OpenGLGraphics/external/imgui/imgui_draw.cpp" "CMakeFiles/GameEngine.dir/external/imgui/imgui_draw.cpp.o" "gcc" "CMakeFiles/GameEngine.dir/external/imgui/imgui_draw.cpp.o.d"
+  "/home/harrison/OpenGLGraphics/external/imgui/imgui_impl_glfw.cpp" "CMakeFiles/GameEngine.dir/external/imgui/imgui_impl_glfw.cpp.o" "gcc" "CMakeFiles/GameEngine.dir/external/imgui/imgui_impl_glfw.cpp.o.d"
+  "/home/harrison/OpenGLGraphics/external/imgui/imgui_impl_opengl3.cpp" "CMakeFiles/GameEngine.dir/external/imgui/imgui_impl_opengl3.cpp.o" "gcc" "CMakeFiles/GameEngine.dir/external/imgui/imgui_impl_opengl3.cpp.o.d"
+  "/home/harrison/OpenGLGraphics/external/imgui/imgui_widgets.cpp" "CMakeFiles/GameEngine.dir/external/imgui/imgui_widgets.cpp.o" "gcc" "CMakeFiles/GameEngine.dir/external/imgui/imgui_widgets.cpp.o.d"
+  "/home/harrison/OpenGLGraphics/external/implot/implot.cpp" "CMakeFiles/GameEngine.dir/external/implot/implot.cpp.o" "gcc" "CMakeFiles/GameEngine.dir/external/implot/implot.cpp.o.d"
+  "/home/harrison/OpenGLGraphics/external/implot/implot_demo.cpp" "CMakeFiles/GameEngine.dir/external/implot/implot_demo.cpp.o" "gcc" "CMakeFiles/GameEngine.dir/external/implot/implot_demo.cpp.o.d"
+  "/home/harrison/OpenGLGraphics/external/implot/implot_items.cpp" "CMakeFiles/GameEngine.dir/external/implot/implot_items.cpp.o" "gcc" "CMakeFiles/GameEngine.dir/external/implot/implot_items.cpp.o.d"
+  "/home/harrison/OpenGLGraphics/src/Camera.cpp" "CMakeFiles/GameEngine.dir/src/Camera.cpp.o" "gcc" "CMakeFiles/GameEngine.dir/src/Camera.cpp.o.d"
+  "/home/harrison/OpenGLGraphics/src/Engine.cpp" "CMakeFiles/GameEngine.dir/src/Engine.cpp.o" "gcc" "CMakeFiles/GameEngine.dir/src/Engine.cpp.o.d"
+  "/home/harrison/OpenGLGraphics/src/Entity.cpp" "CMakeFiles/GameEngine.dir/src/Entity.cpp.o" "gcc" "CMakeFiles/GameEngine.dir/src/Entity.cpp.o.d"
+  "/home/harrison/OpenGLGraphics/src/Light.cpp" "CMakeFiles/GameEngine.dir/src/Light.cpp.o" "gcc" "CMakeFiles/GameEngine.dir/src/Light.cpp.o.d"
+  "/home/harrison/OpenGLGraphics/src/RenderingEngine/Mesh.cpp" "CMakeFiles/GameEngine.dir/src/RenderingEngine/Mesh.cpp.o" "gcc" "CMakeFiles/GameEngine.dir/src/RenderingEngine/Mesh.cpp.o.d"
+  "/home/harrison/OpenGLGraphics/src/RenderingEngine/Renderer.cpp" "CMakeFiles/GameEngine.dir/src/RenderingEngine/Renderer.cpp.o" "gcc" "CMakeFiles/GameEngine.dir/src/RenderingEngine/Renderer.cpp.o.d"
+  "/home/harrison/OpenGLGraphics/src/Shader.cpp" "CMakeFiles/GameEngine.dir/src/Shader.cpp.o" "gcc" "CMakeFiles/GameEngine.dir/src/Shader.cpp.o.d"
+  "/home/harrison/OpenGLGraphics/src/Texture.cpp" "CMakeFiles/GameEngine.dir/src/Texture.cpp.o" "gcc" "CMakeFiles/GameEngine.dir/src/Texture.cpp.o.d"
+  "/home/harrison/OpenGLGraphics/src/Timer.cpp" "CMakeFiles/GameEngine.dir/src/Timer.cpp.o" "gcc" "CMakeFiles/GameEngine.dir/src/Timer.cpp.o.d"
+  "/home/harrison/OpenGLGraphics/src/main.cpp" "CMakeFiles/GameEngine.dir/src/main.cpp.o" "gcc" "CMakeFiles/GameEngine.dir/src/main.cpp.o.d"
   )
 
 # Targets to which this target links.
